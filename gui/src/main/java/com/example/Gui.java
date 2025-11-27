97d11943564f47ec123358fd2e4b869f6d2d2cc9
dr.rivera2003@gmail.com <dr.rivera2003@gmail.com>
2025-11-27 04:01:52 -0700

    GUI
    Now have the top bar and the words for the top half of the gui

--- gui/src/main/java/com/example/Gui.java
+++ gui/src/main/java/com/example/Gui.java
@@ -1,22 +1,53 @@
 package com.example;
 
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
+import javafx.application.*;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.*;
+import javafx.geometry.*;
+import javafx.scene.text.*;
+import javafx.scene.control.*;
+import javafx.scene.paint.*;
+
 
 
 public class Gui extends Application {
      @Override
     public void start(Stage stage) {
+        //Title of the whole thing
+        stage.setTitle("Scholar Cats");
+
+        BorderPane borderPane = new BorderPane();
+      
+        //Header
+
+        HBox topBar = new HBox();
+        topBar.setStyle("-fx-background-color: #001148ff;");
+        topBar.setPrefHeight(100);
+
+       
+        Label header = new Label("Scholar Cats");
+        header.setFont(Font.font("Arial", FontWeight.BOLD, 24));
+        header.setTextFill(Color.RED);
+        
+        topBar.getChildren().add(header);
+        topBar.setAlignment(Pos.CENTER_LEFT);
+        topBar.setPadding(new Insets(10,0,0,10));
+        
+        borderPane.setTop(topBar);
+   
+        
+
+
+        
         Button button = new Button("Click me!");
         button.setOnAction(e -> System.out.println("Button clicked"));
 
         StackPane root = new StackPane(button);
-        Scene scene = new Scene(root, 400, 300);
+        Scene scene = new Scene(borderPane, 600, 600);
 
-        stage.setTitle("JavaFX Button");
+        stage.setTitle("ScholarShip Tracker");
         stage.setScene(scene);
         stage.show();
     }
