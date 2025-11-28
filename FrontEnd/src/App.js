c0b5d571b46a8392969ecb3fd8834841073f63e0
dr.rivera2003@gmail.com <dr.rivera2003@gmail.com>
2025-11-27 20:02:43 -0700

    GUI
    Finished the frame of the GUI

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -42,15 +42,60 @@ function App(){
    <button type ="submit">Search</button>
 </form>
 
-
+{/* Top horizoantal boxes*/}
 <div class = "Top-Horziontal-Boxes">
-<div class = "Top-Box">Total Avaliable</div>
-<div class = "Top-Box">Total Avaliable2</div>
-<div class = "Top-Box">Total Avaliable3</div>
-
-
-  
+<div class = "Top-Box">Total Avaliable
+  <p> Some number that we will find out later </p>
 </div>
+
+<div class = "Top-Box">Your Applications
+<p> Some number that will also be found later</p>
+</div>
+
+<div class = "Top-Box">Match Rate
+  <p> Some number that will also be found later</p>
+</div>
+
+</div>
+
+
+<p>Avaliable ScholarShips</p>
+
+<div class = "Scholarship-list">
+
+<div class ="Scholarship">
+<p>Scholar ship one</p>
+<span className='status-badge open'>Open</span>
+<button className='scholar-ship-button'>View</button>
+<div className='money-info'> $$$$$$</div>
+<div className = 'date-info'>augesu 74</div>
+</div>
+
+<div class ="Scholarship">
+<p>Scholar ship two</p>
+<span className='status-badge closed'>Closed</span>
+<button className='scholar-ship-button'>View</button>
+<div className='money-info'> $$$$$$</div>
+<div className = 'date-info'>augesu 74</div>
+</div>
+
+<div class ="Scholarship">
+<p>Scholar ship three</p>
+<span className='status-badge soon'>Closing soon</span>
+<button className='scholar-ship-button'>View</button>
+<div className='money-info'> $$$$$$</div>
+<div className = 'date-info'>augesu 74</div>
+</div>
+
+<div class ="Scholarship">
+<p>Scholar ship four</p>
+<button className='scholar-ship-button'>View</button>
+<div className='money-info'> $$$$$$</div>
+<div className = 'date-info'>augesu 74</div>
+</div>
+</div>
+
+
 
 
 
