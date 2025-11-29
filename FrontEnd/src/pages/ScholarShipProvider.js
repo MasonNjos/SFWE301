032f678e5f55e3864edbda3a281ca8f31ce835af
dr.rivera2003@gmail.com <dr.rivera2003@gmail.com>
2025-11-28 19:20:14 -0700

    gui
    gui update

--- FrontEnd/src/pages/ScholarShipProvider.js
+++ FrontEnd/src/pages/ScholarShipProvider.js
@@ -5,7 +5,13 @@ return(
 <div className="ScholarShipProviderPage">
 
 <main className ='content-aera'>
+
+<div className ='Basic Information'>
+    <h1>Basic Information</h1>
+    <p>Lets start with some basic information about the Scholarship</p>
     
+</div>
+
 </main>
 
 
